1bd129679aa523d56927b40cdde8b8b94629b864
Mitchell Tillman <mtillman14@gmail.com>
2025-02-04 17:15:29 -0600

    Halfway through reorganizing the data processing into tabular form.
    I have lots of redundant copies of the data. Maybe I just don't combine the tables until the end/when I need to?

--- Ameen_EMG_kinematics/new_main_tables.m
+++ Ameen_EMG_kinematics/new_main_tables.m
@@ -62,3 +62,79 @@ syncedTableDelsys = timeSynchronize(trialTable, delsysConfig.SAMPLING_FREQUENCY,
 syncedTableXSENS = timeSynchronize(trialTable, xsensConfig.SAMPLING_FREQUENCY, 'seconds', 'XSENS_Frames');
 trialTable = addToTable(trialTable, syncedTableDelsys);
 trialTable = addToTable(trialTable, syncedTableXSENS);
+
+%% Plot each trial's data individually, along with gait event information.
+if plot
+    baseSavePathEMG = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\EMG\Trials_GaitEvents';
+    plotTrialWithGaitEvents(trialTable, 'Filtered EMG and Gait Events', baseSavePathEMG, 'Delsys_Filtered', 'Delsys_Frames');
+    baseSavePathXSENS = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\Joint Angles\Trials_GaitEvents';
+    plotTrialWithGaitEvents(trialTable, 'Filtered Joint Angles and GaitEvents', baseSavePathXSENS, 'XSENS_Filtered', 'XSENS_Frames');
+end
+
+%% Split by gait cycle
+
+%% Plot each gait cycle's filtered data, non-time normalized and each gait cycle of one condition plotted on top of each other.
+if plot
+    baseSavePathEMG = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\EMG\Filtered_GaitCycles';
+    plotAllTrials(delsysStruct, 'Filtered EMG', baseSavePathEMG, 'Filtered');
+    baseSavePathXSENS = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\Joint Angles\Filtered_GaitCycles';
+    plotAllTrials(xsensStruct, 'Filtered Joint Angles', baseSavePathXSENS, 'Filtered');
+end
+
+%% Downsample each gait cycle's data to 101 points and aggregate together, within and across trials.
+
+%% Plot each gait cycle's time-normalized data, and each gait cycle of one condition plotted on top of each other.
+if plot
+    baseSavePathEMG = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\EMG\TimeNormalized_GaitCycles';
+    plotAllTrials(delsysStruct, 'Time-Normalized EMG', baseSavePathEMG, 'TimeNormalized');
+    baseSavePathXSENS = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\Joint Angles\TimeNormalized_GaitCycles';
+    plotAllTrials(xsensStruct, 'Time-Normalized Joint Angles', baseSavePathXSENS, 'TimeNormalized');
+end
+
+%% Identify the max EMG data value across one whole visit (all trials & gait cycles)
+
+%% Normalize the time-normalized EMG data to the max value across one whole visit (all trials & gait cycles)
+
+%% Plot each gait cycle's scaled to max EMG data, and each gait cycle of one condition plotted on top of each other.
+if plot
+    baseSavePathEMG = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\EMG\ScaledToMax_GaitCycles';
+    plotAllTrials(delsysStruct, 'Scaled To Max EMG', baseSavePathEMG, 'ScaledToMax');    
+end
+
+%% Set up muscle & joint names for analyses
+disp('Defining L & R names');
+musclesLR = delsysConfig.MUSCLES;
+jointsLR = xsensConfig.JOINTS;
+musclesL = cell(size(musclesLR));
+musclesR = cell(size(musclesLR));
+jointsL = cell(size(jointsLR));
+jointsR = cell(size(jointsLR));
+for i = 1:length(musclesLR)
+    musclesL{i} = ['L' musclesLR{i}];
+    musclesR{i} = ['R' musclesLR{i}];
+end
+for i = 1:length(jointsLR)
+    jointsL{i} = ['L' jointsLR{i}];
+    jointsR{i} = ['R' jointsLR{i}];
+end
+
+%% Calculate the number of muscle synergies in each gait cycle of each trial
+disp('Computing the number of muscle synergies');
+config = jsondecode(fileread(configFilePath));
+VAFthresh = config.DELSYS_EMG.VAF_THRESHOLD;
+
+%% Scatter plot the number of muscle synergies & the step lengths
+if plot
+    baseSavePathEMG = 'Y:\LabMembers\MTillman\GitRepos\Stroke-R01\Plots\EMG\NumSynergies';
+    scatterPlotPerGaitCyclePerIntervention(delsysStruct, 'Num Synergies', baseSavePathEMG, 'NumSynergies');    
+end
+
+%% SPM Analysis for EMG & XSENS
+disp('Running SPM analysis');
+
+%% Calculate the magnitude and duration of L vs. R differences obtained from SPM
+disp('Calculating magnitude & durations of L vs. R differences from SPM');
+
+%% Save the structs to the participant's save folder.
+save(subjectSavePath, 'delsysStruct','gaitRiteStruct','xsensStruct','-v6');
+disp(['Saved ' subject ' structs to: ' subjectSavePath]);
--- Ameen_EMG_kinematics/plotting/plotTrialWithGaitEvents.m
+++ Ameen_EMG_kinematics/plotting/plotTrialWithGaitEvents.m
@@ -1,4 +1,4 @@
-function [] = plotTrialWithGaitEvents(allDataStruct, figName, baseSavePath, plotFieldName)
+function [] = plotTrialWithGaitEvents(allDataTable, figName, baseSavePath, plotFieldName, framesFieldName)
 
 %% PURPOSE: PLOT THE ENTIRETY OF EACH TRIAL ALONG WITH THE GAIT EVENTS.
 % Inputs:
@@ -7,42 +7,31 @@ function [] = plotTrialWithGaitEvents(allDataStruct, figName, baseSavePath, plot
 % name and the prefix of the plot titles.
 % baseSavePath: The folder to save all of the plots to.
 % plotFieldName: The field that is being plotted.
+% framesFieldName: The field used for frame numbers.
 
 figOneTrial = figure('Name',figName);
 figOneTrial.WindowState = 'maximized';
-intervention_field_names = fieldnames(allDataStruct);
 figOneTrialConfig.color = 'k';
-for i = 1:length(intervention_field_names)
-    intervention_field_name = intervention_field_names{i};
-    speedNames = fieldnames(allDataStruct.(intervention_field_name));
-    for speedNum = 1:length(speedNames)
-        speedName = speedNames{speedNum};
-        prePosts = fieldnames(allDataStruct.(intervention_field_name).(speedName));
-        for prePostNum = 1:length(prePosts)
-            prePost = prePosts{prePostNum};
-            trialNames = fieldnames(allDataStruct.(intervention_field_name).(speedName).(prePost).Trials);     
-            for trialNum = 1:length(trialNames)
-                trialName = trialNames{trialNum};
-                clf;
-                trialData = allDataStruct.(intervention_field_name).(speedName).(prePost).Trials.(trialName).(plotFieldName);
-                figOneTrialConfig.title = [figName ': ' intervention_field_name ' ' speedName ' ' prePost ' ' trialName];
-                figOneTrial = plotOneTrialData(trialData, figOneTrial, figOneTrialConfig);                
-                %% Plot the gait phases & events
-                axHandles = findobj(figOneTrial, 'Type', 'axes');
-                framesStruct = allDataStruct.(intervention_field_name).(speedName).(prePost).Trials.(trialName).frames;
-                for axNum = 1:length(axHandles)
-                    ax = axHandles(axNum);
-                    plotGaitEvents(framesStruct, ax);
-                end
-                % Save the plot.
-                saveFolderPath = baseSavePath;
-                mkdir(saveFolderPath);
-                savePath = fullfile(saveFolderPath, [intervention_field_name '_' prePost '_' speedName '_' trialName]);
-                saveas(figOneTrial, [savePath '.fig']);
-                saveas(figOneTrial, [savePath '.png']);
-                saveas(figOneTrial, [savePath '.svg']);
-            end
-        end
+for i = 1:height(allDataTable)
+    clf;
+    name = char(allDataTable.Name(i));
+    displayName = strrep(name, '_', ' ');
+    trialData = allDataTable.(plotFieldName)(i);
+    figOneTrialConfig.title = [figName ': ' displayName];
+    figOneTrial = plotOneTrialData(trialData, figOneTrial, figOneTrialConfig);
+    %% Plot the gait phases & events
+    axHandles = findobj(figOneTrial, 'Type', 'axes');
+    framesStruct = allDataTable.(framesFieldName)(i);
+    for axNum = 1:length(axHandles)
+        ax = axHandles(axNum);
+        plotGaitEvents(framesStruct, ax);
     end
+    % Save the plot.
+    saveFolderPath = baseSavePath;
+    mkdir(saveFolderPath);
+    savePath = fullfile(saveFolderPath, name);
+    saveas(figOneTrial, [savePath '.fig']);
+    saveas(figOneTrial, [savePath '.png']);
+    saveas(figOneTrial, [savePath '.svg']);
 end
 close(figOneTrial);
