985ee8983882025376a89cb8dda0651c026ff22e
Mitchell Tillman <mtillman14@gmail.com>
2025-10-15 14:42:54 -0500

    Compeleted tSCS parameters ANOVAs

--- src/stats/csv-stats.py
+++ src/stats/csv-stats.py
@@ -1,7 +1,7 @@
 # %%
 from pathlib import Path
 
-from csv_stats.anova import anova1way
+from csvstats.anova import anova1way
 import pandas as pd
 
 # %%
@@ -24,45 +24,57 @@ group_column_name = 'Intervention'
 
 # %%
 # CGAM Cohen's d ANOVA
-data_path = r"Y:\LabMembers\MTillman\SavedOutcomes\StrokeSpinalStim\Overground_EMG_Kinematics\MergedTablesAffectedUnaffected\matchedCyclesCGAMCohens.csv"
+data_path = r"~/mnt/rto/LabMembers/MTillman/SavedOutcomes/StrokeSpinalStim/Overground_EMG_Kinematics/MergedTablesAffectedUnaffected/matchedCyclesCGAMCohens.csv"
 df = pd.read_csv(data_path)
 column_names = df.columns.tolist()
 results = {}
-root_save_folder = Path(r"Y:\LabMembers\MTillman\GitRepos\Stroke-R01\results\stats\ANOVA results\between_interventions_cohensd_with_sham2")
+root_save_folder = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_with_sham2")
+root_save_folder_no_sham2 = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_without_sham2")
 column_names = [col for col in column_names if col not in columns_to_drop]
+df_nosham2 = df[df['Intervention'] != 'SHAM2']
 for col in column_names:
     save_file_path = root_save_folder / f"{col}_anova1way.pdf"
     results[col] = anova1way(df, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path)
+    save_file_path_no_sham2 = root_save_folder_no_sham2 / f"{col}_anova1way.pdf"
+    anova1way(df_nosham2, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path_no_sham2)
 
 # %%
 ## Matched Cycles Cohen's d ANOVA
-data_path = r"Y:\LabMembers\MTillman\GitRepos\Stroke-R01\results\stats\Cohensd_CSVs\cohensd_matchedCycles.csv"
+data_path = r"~/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/Cohensd_CSVs/cohensd_matchedCycles.csv"
 df = pd.read_csv(data_path)
 column_names = df.columns.tolist()
 results = {}
-root_save_folder = Path(r"Y:\LabMembers\MTillman\GitRepos\Stroke-R01\results\stats\ANOVA results\between_interventions_cohensd_with_sham2")
+root_save_folder = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_with_sham2")
+root_save_folder_no_sham2 = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_without_sham2")
 column_names = [col for col in column_names if col not in columns_to_drop]
 # Drop rows with "SHAM1" in the Intervention column
 df = df[df['Intervention'] != 'SHAM1']
+df_nosham2 = df[df['Intervention'] != 'SHAM2']
 # Drop rows with NaN values in any of the columns to be analyzed
 df = df.dropna(subset=column_names)
 for col in column_names:
     save_file_path = root_save_folder / f"{col}_anova1way.pdf"
     results[col] = anova1way(df, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path)
+    save_file_path_no_sham2 = root_save_folder_no_sham2 / f"{col}_anova1way.pdf"
+    anova1way(df_nosham2, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path_no_sham2)
 
 # %%
 ## Unmatched Cycles Cohen's d ANOVA
-data_path = r"Y:\LabMembers\MTillman\GitRepos\Stroke-R01\results\stats\Cohensd_CSVs\cohensd_unmatchedCycles.csv"
+data_path = r"~/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/Cohensd_CSVs/cohensd_unmatchedCycles.csv"
 df = pd.read_csv(data_path)
 column_names = df.columns.tolist()
 results = {}
-root_save_folder = Path(r"Y:\LabMembers\MTillman\GitRepos\Stroke-R01\results\stats\ANOVA results\between_interventions_cohensd_with_sham2")
+root_save_folder = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_with_sham2")
+root_save_folder_no_sham2 = Path(r"/home/mtillman/mnt/rto/LabMembers/MTillman/GitRepos/Stroke-R01/results/stats/ANOVA results/between_interventions_cohensd_without_sham2")
 column_names = [col for col in column_names if col not in columns_to_drop]
 # Drop rows with "SHAM1" in the Intervention column
 df = df[df['Intervention'] != 'SHAM1']
+df_nosham2 = df[df['Intervention'] != 'SHAM2']
 # Drop rows with NaN values in any of the columns to be analyzed
 df = df.dropna(subset=column_names)
 for col in column_names:
     save_file_path = root_save_folder / f"{col}_anova1way.pdf"
     results[col] = anova1way(df, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path)
+    save_file_path_no_sham2 = root_save_folder_no_sham2 / f"{col}_anova1way.pdf"
+    anova1way(df_nosham2, group_column_name, col, repeated_measures_column=repeated_measures_column, filename=save_file_path_no_sham2)
 # %%
