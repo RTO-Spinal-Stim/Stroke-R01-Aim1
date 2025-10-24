d34c5d047388abecb9135f23b7fb31e8660b0917
Mitchell Tillman <mtillman14@gmail.com>
2025-10-24 16:37:48 -0500

    Best and worst stim vs. sham t-test

--- src/stats/csv-stats.py
+++ src/stats/csv-stats.py
@@ -7,7 +7,7 @@ from csvstats.anova import anova1way
 from csvstats.ttest import ttest_dep, ttest_ind
 import pandas as pd
 
-from select_best_stim import select_best_stim
+from select_one_stim import select_best_stim, select_worst_stim
 
 # %%
 ## Configuration
@@ -57,7 +57,22 @@ for speed in speeds:
         # %% Get the best stim
         data_path = f"results/stats/Cohensd_CSVs/cohensd_{table_name}_{speed}.csv"
         all_df = pd.read_csv(data_path)
+        sham_df = all_df[all_df["Intervention"] == "SHAM2"]
         best_stim_df = select_best_stim(all_df)
+        worst_stim_df = select_worst_stim(all_df)
+        # Perform the best/worst SHAM vs. STIM t-test
+        best_stim_and_sham_df = pd.concat([sham_df, best_stim_df])
+        best_stim_save_path = "results/stats/ttest_results/best_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        results = ttest_dep(best_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
+        worst_stim_and_sham_df = pd.concat([sham_df, worst_stim_df])
+        worst_stim_save_path = "results/stats/ttest_results/worst_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        results = ttest_dep(worst_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
+        save_path_best_stim = "results/stats/ttest_results/best_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        # Perform one sample best and worst STIM t-test
+        results = ttest_ind(best_stim_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
+        save_path_worst_stim = "results/stats/ttest_results/worst_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        results = ttest_ind(worst_stim_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
+
 
         ##################################################################
         #################### Session Order ANOVA #########################
--- src/stats/select_one_stim.py
+++ src/stats/select_one_stim.py
@@ -10,3 +10,14 @@ def select_best_stim(df: pd.DataFrame, column: str) -> pd.DataFrame:
     
     # Return the rows corresponding to those indices
     return df.loc[idx]
+
+
+def select_worst_stim(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Return a data frame with one entry (one intervention) per subject with the intervention with the lowest value in the specified column"""
+
+    subject_col = "Subject"
+
+    # Find the index of the row with the minimum value in the specified column for each subject
+    idx = df.groupby(subject_col)[column].idxmin()
+
+    return df.loc[idx]
