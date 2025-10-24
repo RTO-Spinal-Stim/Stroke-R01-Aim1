bfc3bada9e0de5106ee2d3bcf8e2a143657f1c2b
[Mitchell Tillman] <[mtillman14@gmail.com]>
2025-10-24 18:59:19 -0500

    Successfully ran on my Mac

--- src/stats/csv-stats.py
+++ src/stats/csv-stats.py
@@ -29,8 +29,10 @@ for speed in speeds:
         # %% CGAM Cohen's d ANOVA
         data_path = f"results/stats/Cohensd_CSVs/cohensd_{table_name}_{speed}.csv"
         df = pd.read_csv(data_path)
-        save_path = Path("results/stats/ANOVA results/between_interventions_cohensd_with_sham2/symmetry/{{data_column}}_{speed}_anova1way.pdf".format(speed=speed))
+        save_path = Path("results/stats/ANOVA results/between_interventions_cohensd_with_sham2/symmetry/{{data_column}}_{speed}_anova1way.pdf".format(speed=speed))        
         root_save_folder_no_sham2 = Path("results/stats/ANOVA results/between_interventions_cohensd_without_sham2/symmetry/{{data_column}}_{speed}_anova1way.pdf".format(speed=speed))
+        save_path.parent.mkdir(parents=True, exist_ok=True)
+        root_save_folder_no_sham2.parent.mkdir(parents=True, exist_ok=True)
         df_nosham2 = df[df['Intervention'] != 'SHAM2']
         # df = df.drop(['SessionOrder', 'Frequency'], axis=1)
         results_sham2 = anova1way(df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)    
@@ -43,12 +45,15 @@ for speed in speeds:
         data_path = f"results/stats/Cohensd_ttest_CSVs/cohensd_{table_name}_{speed}.csv"
         df = pd.read_csv(data_path)
         save_path = Path("results/stats/ttest_results/sham_vs_stim/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+        save_path.parent.mkdir(parents=True, exist_ok=True)
         results = ttest_dep(df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
         df_sham = df[df['Intervention'] == 'SHAM']
-        save_path_sham = "results/stats/ttest_results/sham_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        save_path_sham = Path("results/stats/ttest_results/sham_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+        save_path_sham.parent.mkdir(parents=True, exist_ok=True)
         results = ttest_ind(df_sham, group_column_name, "_", filename=save_path_sham)
         df_stim = df[df['Intervention'] == 'STIM']
-        save_path_stim = "results/stats/ttest_results/stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
+        save_path_stim = Path("results/stats/ttest_results/stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+        save_path_stim.parent.mkdir(parents=True, exist_ok=True)
         results = ttest_ind(df_stim, group_column_name, "_", filename=save_path_stim)
 
         ##################################################################
@@ -57,21 +62,30 @@ for speed in speeds:
         # %% Get the best stim
         data_path = f"results/stats/Cohensd_CSVs/cohensd_{table_name}_{speed}.csv"
         all_df = pd.read_csv(data_path)
-        sham_df = all_df[all_df["Intervention"] == "SHAM2"]
-        best_stim_df = select_best_stim(all_df)
-        worst_stim_df = select_worst_stim(all_df)
-        # Perform the best/worst SHAM vs. STIM t-test
-        best_stim_and_sham_df = pd.concat([sham_df, best_stim_df])
-        best_stim_save_path = "results/stats/ttest_results/best_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
-        results = ttest_dep(best_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
-        worst_stim_and_sham_df = pd.concat([sham_df, worst_stim_df])
-        worst_stim_save_path = "results/stats/ttest_results/worst_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
-        results = ttest_dep(worst_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
-        save_path_best_stim = "results/stats/ttest_results/best_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
-        # Perform one sample best and worst STIM t-test
-        results = ttest_ind(best_stim_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
-        save_path_worst_stim = "results/stats/ttest_results/worst_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed)
-        results = ttest_ind(worst_stim_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=save_path)
+        sham_df = all_df[all_df["Intervention"] == "SHAM2"].copy()
+        sham_df["Intervention"] = "SHAM"
+        all_df_columns = all_df.columns.drop([group_column_name, repeated_measures_column])
+        for column in all_df_columns:
+            best_stim_df = select_best_stim(all_df, column).copy()
+            best_stim_df["Intervention"] = "BEST_STIM"
+            worst_stim_df = select_worst_stim(all_df, column).copy()
+            worst_stim_df["Intervention"] = "WORST_STIM"
+            # Perform the best/worst SHAM vs. STIM t-test
+            best_stim_and_sham_df = pd.concat([sham_df, best_stim_df])
+            best_stim_vs_sham_save_path = Path("results/stats/ttest_results/best_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+            best_stim_vs_sham_save_path.parent.mkdir(parents=True, exist_ok=True)
+            results = ttest_dep(best_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=best_stim_vs_sham_save_path)
+            worst_stim_and_sham_df = pd.concat([sham_df, worst_stim_df])
+            worst_stim_vs_sham_save_path = Path("results/stats/ttest_results/worst_stim_vs_sham/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+            worst_stim_vs_sham_save_path.parent.mkdir(parents=True, exist_ok=True)
+            results = ttest_dep(worst_stim_and_sham_df, group_column_name, "_", repeated_measures_column=repeated_measures_column, filename=worst_stim_vs_sham_save_path)            
+            # Perform one sample best and worst STIM t-test
+            save_path_best_stim = Path("results/stats/ttest_results/best_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+            save_path_best_stim.parent.mkdir(parents=True, exist_ok=True)
+            results = ttest_ind(best_stim_df, group_column_name, column, filename=save_path_best_stim)
+            save_path_worst_stim = Path("results/stats/ttest_results/worst_stim_vs_zero/symmetry/{{data_column}}_{speed}_ttest.pdf".format(speed=speed))
+            save_path_worst_stim.parent.mkdir(parents=True, exist_ok=True)
+            results = ttest_ind(worst_stim_df, group_column_name, column, filename=save_path_worst_stim)
 
         ##################################################################
         #################### Session Order ANOVA #########################
